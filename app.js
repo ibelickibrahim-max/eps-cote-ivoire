432fa1642ea2cd284753bbc8dcccb75bb42d41fa
ibelickibrahim-max <ibelickibrahim@gmail.com>
2026-09-19 13:03:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("🔎 Recherche\n\nLa recherche sera bientôt disponible.");
       }
 
-  
 else if (section.includes("ressources")) {
   document.getElementById("ressources")?.scrollIntoView({ behavior: "smooth" });
 }
